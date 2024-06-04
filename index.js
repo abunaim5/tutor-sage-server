1b532e724c13d089a4ddb3a1ed93c9b1af35ee41
Abu Naim <abunaim140@gmail.com>
2024-06-04 22:33:07 +0600

    added delete a user api with delete method

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
       res.send(result)
     });
 
+    app.delete('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // user related api
     app.get('/classes', async (req, res) => {
       const result = await classCollection.find().toArray();
