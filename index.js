1624fc1d3b08fe6f9810fcb2ce61d97367ff1a63
Abu Naim <abunaim140@gmail.com>
2024-06-06 00:23:01 +0600

    implemented class delete method api

--- index.js
+++ index.js
@@ -94,16 +94,23 @@ async function run() {
     });
 
     // teachers related apis
+    app.get('/myClasses/:email', async(req, res) => {
+      const email = req.params.email;
+      const query = {email: email};
+      const result = await classCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post('/classes', async(req, res) => {
       const classData = req.body;
       const result = await classCollection.insertOne(classData);
       res.send(result);
     });
 
-    app.get('/myClasses/:email', async(req, res) => {
-      const email = req.params.email;
-      const query = {email: email};
-      const result = await classCollection.find(query).toArray();
+    app.delete('/classes/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await classCollection.deleteOne(query);
       res.send(result);
     });
 
