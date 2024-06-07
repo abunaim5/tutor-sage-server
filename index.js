2596b205ef588f72ed9eed8d63309f817b027787
Abu Naim <abunaim140@gmail.com>
2024-06-07 20:13:35 +0600

    implement find enroll classes by specific id api

--- index.js
+++ index.js
@@ -181,6 +181,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/enrollClasses/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await enrollClassCollection.findOne(query);
+      res.send(result);
+    });
+
     app.get('/enrollClasses/:email', async (req, res) => {
       const email = req.params.email;
       const query = { user_email: email };
